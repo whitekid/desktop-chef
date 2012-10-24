a07626396b36ee3c9486ab68bdeff082b62c180b
whitekid <whitekid@gmail.com>
2012-10-25 02:10:45 +0900

    update vim settings

--- cookbooks/desktop/recipes/default.rb
+++ cookbooks/desktop/recipes/default.rb
@@ -7,3 +7,17 @@
 # All rights reserved - Do Not Redistribute
 #
 include_recipe "nginx"
+
+template "/etc/vimrc.local" do
+	mode "0644"
+	source "vimrc.local.erb"
+end
+
+package "vim"
+
+template "/etc/profile.d/vim_alias.sh" do
+	mode "0644"
+	source "vim_alias.sh.erb"
+end
+
+# vim: nu ai ts=4 tw=4
