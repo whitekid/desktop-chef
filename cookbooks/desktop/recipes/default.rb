57d49953f048c67d5db364bf46075381da87ed67
whitekid <whitekid@gmail.com>
2012-10-25 12:15:27 +0900

    add nginx proxy

--- cookbooks/desktop/recipes/default.rb
+++ cookbooks/desktop/recipes/default.rb
@@ -6,13 +6,9 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-include_recipe "nginx"
 
-template "/etc/vimrc.local" do
-	mode "0644"
-	source "vimrc.local.erb"
-end
 
+# vim
 package "vim"
 
 template "/etc/profile.d/vim_alias.sh" do
@@ -20,4 +16,45 @@ template "/etc/profile.d/vim_alias.sh" do
 	source "vim_alias.sh.erb"
 end
 
-# vim: nu ai ts=4 tw=4
+template "/etc/vimrc.local" do
+	mode "0644"
+	source "vimrc.local.erb"
+end
+
+# nginx
+include_recipe "nginx"
+
+template "/etc/nginx/sites-available/mirror-8080" do
+	mode "0644"
+	source "nginx_mirror-8080.erb"
+end
+
+nginx_site "mirror-8080" do
+	enable true
+end
+
+template "/etc/nginx/sites-available/chef-proxy-4040" do
+	mode "0644"
+	source "nginx_chef-proxy.erb"
+end
+
+nginx_site "chef-proxy-4040" do
+	enable true
+end
+
+# my scripts
+directory "/home/choe/bin" do
+	owner "choe"
+	group "choe"
+end
+
+%w{init_chef.sh  mirror-centos.sh  mirror-ubuntu-release.sh  mirror.sh  openstack_manual.sh  screen.sh  sshpass.sh  vmproj.sh}.each do |f|
+	template "/home/choe/bin/#{f}" do
+		mode "0755"
+		owner "choe"
+		group "choe"
+		source "bin/#{f}"
+	end
+end
+
+# vim: nu ai ts=4 sw=4
